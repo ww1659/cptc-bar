05cef9d4af7e26ded0b0340564a6489528b4e682
Billy White <billywhite@Billys-Mac-mini.local>
2024-04-07 23:22:33 +0100

    changed URL of place order button

--- components/PlaceOrderButton.tsx
+++ components/PlaceOrderButton.tsx
@@ -10,7 +10,7 @@ const PlaceOrderButton: React.FC<{}> = () => {
     console.log("Order Placed", order);
 
     try {
-      const response = await fetch(`http://localhost:3000/api/order`, {
+      const response = await fetch("/api/order", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
